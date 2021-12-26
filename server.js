087930ace9f5d2a4ce330f212a6566b0248eaa70
SrdjanKljajo <websmartist@gmail.com>
2021-12-26 11:38:13 +0100

    change port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(notFound)
 // Custom database errors
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, async () => {
   console.log(`Server run on ${process.env.NODE_ENV} mode, on port ${PORT}`)
